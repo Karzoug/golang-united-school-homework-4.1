0397ce67e265ecf85d5fb7fc40a1cd7cd2dcbcce
Aleksandr Baranov <a.n.baranov@outlook.com>
2022-05-16 21:15:12 +0300

    implemented function ReverseString

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+	res := make([]rune, len(runes))
+
+	for i := len(runes) - 1; i >= 0; i-- {
+		res = append(res, runes[i])
+	}
+	output = string(res)
 	return output
 }
